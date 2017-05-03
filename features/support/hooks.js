3deba3d36a75e1be3bd0552ac5db3b6fca18c0e7
Adam Katona <adam_katona@epam.com>
2017-05-03 17:24:31 +0200

    -adding screenshot

--- features/support/hooks.js
+++ features/support/hooks.js
@@ -8,6 +8,7 @@ var path = require('path');
 var cucumberJunit = require('cucumber-junit');
 //https://www.npmjs.com/package/cucumber-html-reporter
 var reporter = require('cucumber-html-reporter');
+var eachStepScreensArr = [];
 var chai = require('chai'),
     chaiAsPromised = require('chai-as-promised');
 
@@ -25,6 +26,12 @@ var webdriver = require('selenium-webdriver');
 global.by = webdriver.By;
 module.exports = function () {
     this.setDefaultTimeout(60000);
+
+    this.Before(function () {
+        //Reset Array with Step screenshots
+        eachStepScreensArr = [];
+    });
+
     this.registerHandler('BeforeFeatures', function () {
 
         global.driver = new webdriver.Builder().withCapabilities(webdriver.Capabilities.chrome()).build();
@@ -41,8 +48,25 @@ module.exports = function () {
         return global.driver.manage().window().maximize();
     });
 
+    this.registerHandler('StepResult', function (result) {
+        if (result.getStatus() === "failed") {
+            return global.driver.takeScreenshot().then(function (png) {
+                eachStepScreensArr.push(new Buffer(png.replace(/^data:image\/(png|gif|jpeg);base64,/, ''), 'base64'));
+            });
+        }
+        return Promise.resolve();
+    });
+
+    this.After(function (scenario){
+        //Attach any step screenshots to the scenario metadata
+        for (var key in eachStepScreensArr) {
+            scenario.attach(eachStepScreensArr[key], 'image/png');
+        }
+        return Promise.resolve();
+    });
+
     this.registerHandler('AfterFeatures', function () {
-        fs.writeFileSync(path.resolve('./reports/cucumber_junit.xml'),cucumberJunit(JSON.stringify(require('../../reports/cucumber_report.json'))),'utf8');
+        fs.writeFileSync(path.resolve('./reports/cucumber_junit.xml'), cucumberJunit(JSON.stringify(require('../../reports/cucumber_report.json'))), 'utf8');
         reporter.generate(options);
         return global.driver.quit();
     });
